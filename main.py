5b5a31e6e8b2b3485e79085b7480e7667ce0db18
ManhTuan97s <145177973+ManhTuan97s@users.noreply.github.com>
2023-11-01 22:28:08 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,12 @@
+# from custom_data_type import CustomDataType
+
+
+
+HANG_SO_PI = 3.14
+
+bien_so_nguyen = 45 
+print("bien_so_nguyen ban dau",bien_so_nguyen )
+bien_so_nguyen = 70
+print("bien_so_nguyen thay doi gia tri",bien_so_nguyen )
+
+bien_dang_so_nguyen: int = 15
